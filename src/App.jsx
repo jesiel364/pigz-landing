79ff858088d9a8c313e2164de1228858cc0b51a5
jesiel364 <jesiel364@gmail.com>
2023-07-01 04:24:50 -0300

    cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
 import PigzLogo from './assets/logo.svg';
+import Logo2 from './assets/logo2.svg';
 import Burger1 from './assets/burger1/500.png';
+import Store from './assets/icones-banner/store.svg';
+import Smartphone from './assets/icones-banner/smartphone.svg';
+import PigzGestao from './assets/icones-banner/pigz-gestao.svg';
+import Print from './assets/icones-banner/print.svg';
 
 function Navbar() {
   return (
@@ -64,14 +69,60 @@ function Form() {
   );
 }
 
+function Secao(){
+
+  
+  return (
+    <div id="div-meio">
+      <img id="logo2" src={Logo2} />
+
+      <p>Você tem um novo Pigzdido!</p>
+
+      <div className="card-banner">
+        <img src={Store} />
+        <p id="title">Marketplace</p>
+        <p id="desc">Pra sua loja vender mais</p>
+      </div>
+      <div className="card-banner">
+        <img src={Smartphone} />
+        <p id="title">É fácil e rápido</p>
+        <p id="desc">Fazer um pedido no Pigz</p>
+      </div>
+      <div className="card-banner">
+        <img src={PigzGestao} />
+        <p id="title">Pigz Gestão</p>
+        <p id="desc">Você no controle, sempre</p>
+      </div>
+      <div className="card-banner">
+        <img src={Print} />
+        <p id="title">Vias de impressão</p>
+        <p id="desc">Vias de impressão</p>
+      </div>
+      
+    </div>
+  );
+}
+
+function Secao2(){
+  return (
+    <div className="Secao2">
+      <p class='title'>Tudo que você precisa<br></br> por apenas R$199/mês</p>
+      <p class="desc">Tenha todas as funcionalidades<br></br> Pigz sem taxa de adesão, sem<br></br> comissão por cada venda, sem<br></br> letrinhas miúdas.</p>
+    </div>
+  );
+}
+
 export default function App() {
   return (
-    <section>
+    <div className="Wrapper" >
       <Navbar />
       <div className='Div1'>
       <Container1 />
       <Form />
       </div>
-    </section>
+
+      <Secao />
+      <Secao2 />
+    </div>
   );
 }
