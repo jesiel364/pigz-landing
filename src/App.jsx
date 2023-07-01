7af85e18d1e358251129ea9d0ee2564fa5ed7ed8
jesiel364 <jesiel364@gmail.com>
2023-06-30 23:04:47 -0300

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import PigzLogo from './assets/logo.svg';
-import Burger1 from './assets/burger1/250.png';
+import Burger1 from './assets/burger1/500.png';
 
 function Navbar() {
   return (
@@ -9,7 +9,7 @@ function Navbar() {
       <img className="Logo" src={PigzLogo} alt="React Logo" />
       </div>
       <div>
-        <button>Já sou parceiro</button>
+        <button className="Btn">Já sou parceiro</button>
       </div>
     </div>
   );
@@ -20,7 +20,11 @@ function Container1() {
     <div className="Container1">
       <div class="title-div">
         <h2>Pigz: tudo que você precisa pra vender mais!</h2>
-        {/* <img src={Burger1} /> */}
+
+        <div>
+        <img src={Burger1} />
+        </div>
+
       </div>
       <div>
         <p>
@@ -36,17 +40,25 @@ function Form() {
   return (
     <div className="form1">
       <form>
-        <h3>Quero vender no Pigz</h3>
+        <p class="title">Quero vender no Pigz</p>
         <p>Dê o primeiro passo para aumentar as suas vendas.</p>
 
         <div className="Form-input">
           <label>Nome</label><br />
-          <input type="text" />
+          <input type="text" placeholder="Jesiel Gomes"/>
         </div>
         <div className="Form-input" >
           <label>Email</label><br />
-          <input type="email" />
+          <input type="email" placeholder="jesiel@email.com"/>
         </div>
+        <div className="Form-input" >
+          <label>Telefone</label><br />
+          <input type="phone" placeholder="(95) 99875-5432"/>
+        </div>
+
+        <p >Ao continuar, aceito que a Pigz entre em contato comigo por telefone, e-mail ou WhatsApp.</p>
+
+        <button className="Btn">Já sou parceiro</button>
       </form>
     </div>
   );
@@ -56,8 +68,10 @@ export default function App() {
   return (
     <section>
       <Navbar />
+      <div className='Div1'>
       <Container1 />
       <Form />
+      </div>
     </section>
   );
 }
